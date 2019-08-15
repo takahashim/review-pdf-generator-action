25ac46ff16190f6244b961b99ecc1005b8a1a64b
takahashim <maki@rubycolor.org>
2019-08-16 00:04:00 +0900

    fix path

--- copy_build_file.rb
+++ copy_build_file.rb
@@ -4,7 +4,7 @@ require 'date'
 
 OUTPUT_PATH = "build/output.pdf"
 
-config_dir = ""
+config_dir = "."
 
 if ARGV[0]
   config_dir = ARGV[0]
@@ -13,11 +13,11 @@ end
 config_path = File.join(config_dir, "config.yml")
 
 conf = YAML.safe_load(File.read(config_path), [Date])
-pdf_file = conf["bookname"]+".pdf"
-puts "find PDF #{pdf_file}."
+pdf_path = File.join(config_dir, conf["bookname"]+".pdf")
+puts "generate PDF #{pdf_path}."
 
-if File.exist?(pdf_file)
+if File.exist?(pdf_path)
   FileUtils.mkdir_p(File.dirname(OUTPUT_PATH))
-  FileUtils.cp(File.join(config_dir, pdf_file), OUTPUT_PATH)
+  FileUtils.cp(pdf_path, OUTPUT_PATH)
 end
 
